55ca3caa75f1d33d03284914d70408f4f86b291a
lazarenco.victor <vlazarenco@mixbook.com>
2024-04-18 11:42:39 +0300

    30. extracting a substring using bounds / bound+offset

--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -42,7 +42,7 @@ It was the worst of times.
 }
     assert_equal 54, long_string.length
     assert_equal 3, long_string.lines.count
-    assert_equal "\n", long_string[0,1]
+    assert_equal "\n", long_string[0, 1]
   end
 
   def test_here_documents_can_also_handle_multiple_lines
@@ -52,7 +52,7 @@ It was the worst of times.
 EOS
     assert_equal 53, long_string.length
     assert_equal 2, long_string.lines.count
-    assert_equal "I", long_string[0,1]
+    assert_equal "I", long_string[0, 1]
   end
 
   def test_plus_will_concatenate_two_strings
@@ -140,9 +140,9 @@ EOS
   end
 
   def test_you_can_get_a_substring_from_a_string
-    string = "Bacon, lettuce and tomato"
-    assert_equal __, string[7,3]
-    assert_equal __, string[7..9]
+    string = 'Bacon, lettuce and tomato'
+    assert_equal 'let', string[7, 3]
+    assert_equal 'let', string[7..9]
   end
 
   def test_you_can_get_a_single_character_from_a_string
